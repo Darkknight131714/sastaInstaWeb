e0d41ae2854a45300c1a97fbe19a5d31037e31fc
Gautam <darkknight131714@gmail.com>
2023-03-28 20:58:34 +0530

    Hello

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+class App extends React.Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+	// Constructor
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			items: [],
+			DataisLoaded: false
+		};
+	}
+
+	// ComponentDidMount is used to
+	// execute the code
+	componentDidMount() {
+		fetch(
+'https://sastainsta.onrender.com/')
+			.then((res) => res.json())
+			.then((json) => {
+				this.setState({
+					items: json,
+					DataisLoaded: true
+				});
+			})
+	}
+	render() {
+		const { DataisLoaded, items } = this.state;
+		if (!DataisLoaded) return <div>
+			<h1> Pleses wait some time.... </h1> </div> ;
+
+		return (
+		<div className = "App">
+			<h1> Fetch data from an api in react </h1>
+      {
+				items.map((item) => (
+          <>
+            <ol key={item.id}>
+              <li> User_Name: {item.name}</li>
+              <li>User_Email: {item.email}</li>
+
+            </ol>
+            <img src={item.url} />
+          </>
+        ))
+      }
     </div>
-  );
+	);
+}
 }
 
 export default App;
