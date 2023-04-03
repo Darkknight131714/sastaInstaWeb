e9403b96ec6781eba2962e610a0f3b9050b860d1
Shashwat <shashwatmittal.india@gmail.com>
2023-04-03 22:54:49 +0530

    linked pages back to api and comments for using local data from json

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,24 +2,25 @@ import React, {useState, useEffect} from "react";
 import '../App.css';
 import loader from '../assets/loader.gif'
 import title from '../assets/title.svg'
-// import { getPostDetails } from "../services/api";
-import data from '../components/sastainsta.json'
+import { getPostDetails } from "../services/api";
+// import data from '../components/sastainsta.json'
 import Post from '../components/Post.js'
 
 
 const Home = (props) => {
+	
 	const [items, setItems] = useState([]);
 	const [dataIsLoaded, setDataIsLoaded] = useState(false);
     // console.log(data);
 	useEffect(() => {
 	  const getData = async () => {
-		// const data = await getPostDetails();
+		const data = await getPostDetails();
         console.log(data);
 
         
 		// console.log(data);
 		if(data) {
-			setItems(data);
+			setItems(data?.data);
 			setDataIsLoaded(true);
 		}
 	}
@@ -46,8 +47,9 @@ const Home = (props) => {
 						{/* console.log(item) */}
 						return (
 							<Post item={item}/>
-						)})
-					}
+						)
+					})
+				}
 			</div>
 		</>
 	)
